07d01106ab579db279e22af5242fdd6de105a642
BW <sevenvsby2@naver.com>
2022-02-17 22:36:32 +0900

    0217 commit

--- 0217_최병욱.py
+++ 0217_최병욱.py
@@ -100,3 +100,80 @@ for num in arr:
     for n in num:
         print(n, end = ' ')
     print()
+
+# 회문1
+T = int(input())
+for test_case in range(1, T+1):
+    print(f'#{test_case}', end = ' ')
+    N, M = map(int, input().split())
+    words = [list(input()) for _ in range(N)]
+    breaker = False
+    for l in range(N):
+        # 행 우선 순회
+        for i in range(N-M+1):
+            # abcdbca M = 7
+            for j in range(M//2):
+                if words[l][i + j] != words[l][i + M-j-1]:
+                    break
+            else: # 회문을 찾았을 때
+                result = ''
+                for k in range(i, i + M):
+                    result += words[l][k]
+                breaker = True
+                break
+        if breaker:
+            print(result)
+            break
+
+        # 열 우선 순회
+        for i in range(N-M + 1):
+            for j in range(M//2):
+                if words[i + j][l] != words[i + M-j-1][l]:
+                    break
+            else:
+                result = ''
+                for k in range(i, i + M):
+                    result += words[k][l]
+                breaker = True
+                break
+        if breaker:
+            print(result)
+            break
+
+# a = 'bbab' # word -> b -> b -> a -> b
+# for word in a:
+#     if word == 'a':
+#         print('a')
+#         break
+# else:
+#     print('a가 없음')
+
+# 회문2
+T = 10
+for test_case in range(1, T+1):
+    tc = int(input())
+    print(f'#{test_case}', end = ' ')
+    words = [list(input()) for _ in  range(100)]
+    max_len = 0
+    for i in range(100):
+        # 행 우선 순회
+        # ABCCBAANDB i = 0 j = 0 ... 100
+        for j in range(100):
+            for M in range(100 - j + 1):
+                for k in range(M//2):
+                    if words[i][j + k] != words[i][j + M-k-1]:
+                        break
+                else: # 회문을 찾았을 때
+                    if max_len < M:
+                        max_len = M
+        # 열 우선 순회
+        for j in range(100):
+            for M in range(1, 100 - j + 1):
+                for k in range(M//2):
+                    if words[j + k][i] != words[j + M-k-1][i]:
+                        break
+                else:
+                    if max_len < M:
+                        max_len = M
+    print(max_len)
+
