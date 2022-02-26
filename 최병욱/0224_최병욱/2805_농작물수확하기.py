7aaaa1df8bd8eca65225a7d68922277a0bc1e45f
BW <sevenvsby2@naver.com>
2022-02-26 22:21:01 +0900

    0227 commit

--- 최병욱/0224_최병욱/2805_농작물수확하기.py
+++ 최병욱/0224_최병욱/2805_농작물수확하기.py
@@ -13,3 +13,9 @@ for test_case in range(1, T+1):
             total += arr[i][k]
             total += arr[N-1-i][k]
     print(f'#{test_case} {total}')
+
+# 14 0 54 1 N//2 = 5//2 = 2
+# 4 425 0 3 i = 1
+#  02032 5
+# 5 120 4 3 i = 3 = N - 1 - i = 5 - 1 - 1
+# 52 2 12 1 i = 4 = N- 1 - i = 5 - 1- 0
--- 최병욱/0224_최병욱/3499_퍼펙트셔플.py
+++ 최병욱/0224_최병욱/3499_퍼펙트셔플.py
@@ -6,12 +6,12 @@ for test_case in range(1, T+1):
     print(f'#{test_case}', end = ' ')
     N = int(input())
     cards = list(input().split())
-    if N % 2:
+    if N % 2: # 홀수
         for i in range(N//2+1):
             print(cards[i], end = ' ')
             if i + N//2+1 < N:
                 print(cards[i+N//2+1], end = ' ')
-    else:
+    else: # 짝수
         for i in range(N//2):
             print(cards[i], cards[i+N//2], end = ' ')
     print()
