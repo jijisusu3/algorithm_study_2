eb66c2c1ee1ed5ee6a29cce4b3491dceee79d6d1
sungjae <lsuksa1@naver.com>
2022-02-27 09:38:33 +0900

    0227lsj

--- 이성재/0227/1860_진기최고급붕어빵.py
+++ 이성재/0227/1860_진기최고급붕어빵.py
@@ -6,12 +6,39 @@ for tc in range(1, T + 1):
     arr = list(map(int, input().split()))
 
     arr.sort()
+    a = 'Possible'  # 아래에서 'Impossible'만 찾기 위해서
+    cnt = 0   # 빵 개수
+    for i in range(arr[-1] + 1):
+        while i > arr[0] or arr == []:
+            cnt -= 1
+            arr.pop(0)
 
-    result = 'Possible'
-    for i in range(N):
-        cnt = (arr[i] // M) * K
-        if cnt < i + 1:
-            result = 'Impossible'
-            break
+        if i % M == 0 and i != 0:
+            cnt += K
 
-    print('#{} {}'.format(tc, result))
+        if i == arr[0]:
+            if cnt <= 0:
+                a = 'Impossible'
+                break
+            else:
+                cnt -= 1
+                arr.pop(0)
+
+    print('#{} {}'.format(tc, a))
+
+
+# T = int(input())
+# for tc in range(1, T + 1):
+#     N, M, K = map(int, input().split())
+#     arr = list(map(int, input().split()))
+#
+#     arr.sort()
+#
+#     result = 'Possible'
+#     for i in range(N):
+#         cnt = (arr[i] // M) * K
+#         if cnt < i + 1:
+#             result = 'Impossible'
+#             break
+#
+#     print('#{} {}'.format(tc, result))
