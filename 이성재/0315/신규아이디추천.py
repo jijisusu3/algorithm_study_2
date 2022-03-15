192e40d7512c6e84c3b242f1131cf27ade2c09d6
sungjae <lsuksa1@naver.com>
2022-03-15 21:31:02 +0900

    0315lsj

--- 이성재/0315/신규아이디추천.py
+++ 이성재/0315/신규아이디추천.py
@@ -1,19 +1,25 @@
 def solution(new_id):
-    answer = ''
+    ans = ''
 
     new_id = new_id.lower()
 
     id = 'abcdefghijklmnopqrstuvwxyz0123456789-_.'
     for i in new_id:
         if i in id:
+            ans += i
+
+    answer = ans[0]
+    for i in ans[1:]:
+        if i != '.':
+            answer += i
+        elif i == '.' and answer[-1] != '.':
             answer += i
 
-    while '..' in answer:
-        answer = answer.replace('..', '.')
-
-    if answer[0] == '.':
+    if answer[0] == answer[-1] == '.':
+        answer = answer[1:-1]
+    elif answer[0] == '.':
         answer = answer[1:]
-    if answer[-1] == '.':
+    elif answer[-1] == '.':
         answer = answer[:-1]
 
     if answer == '':
